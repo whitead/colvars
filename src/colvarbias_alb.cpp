286c2cc50026fde478d8a3ff0e0561119b8f1d15
Andrew White <white.d.andrew@gmail.com>
2013-08-26 13:18:53 -0500

    Added initial guess

--- src/colvarbias_alb.cpp
+++ src/colvarbias_alb.cpp
@@ -49,6 +49,9 @@ colvarbias_alb::colvarbias_alb(std::string const &conf, char const *key) :
   get_keyval (conf, "outputGradient", b_output_grad, false);
   get_keyval (conf, "outputCoupling", b_output_coupling, true);
 
+  //initial guess
+  get_keyval (conf, "forceConstant", coupling_force, 0.0);
+
   if(cvm::temperature() > 0)
     get_keyval (conf, "couplingRange", max_coupling_change, 3 * cvm::temperature() * cvm::boltzmann());
   else
@@ -117,7 +120,8 @@ cvm::real colvarbias_alb::update() {
     for(size_t i = 0; i < colvars.size(); i++) {
       
       temp = means_cu[i] - means[i] * means_sq[i] - 2. * colvar_centers[i] * means_sq[i] + 2. *
-	colvar_centers[i] * means[i] * means[i];
+      	colvar_centers[i] * means[i] * means[i];
+      //temp = -2. * (means[i] - colvar_centers[i]) * (means_sq[i] - means[i] * means[i]);
       
       if(cvm::temperature() > 0)
 	step_size += temp / (cvm::temperature()  * cvm::boltzmann());
@@ -131,6 +135,7 @@ cvm::real colvarbias_alb::update() {
     
     coupling_force_accum += step_size * step_size;
     coupling_force += max_coupling_change / sqrt(coupling_force_accum) * step_size;
+
 
     update_calls = 0;      
     b_equilibration = true;
